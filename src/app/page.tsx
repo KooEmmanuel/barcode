2dfc936aa5284cf7c27bd18f6fec01aa526feb25
KooEmmanuel <nyatefeemmanuel@gmail.com>
2024-09-17 00:51:04 -0700

    intitial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useBarcode } from 'react-barcodes';
-// @ts-ignore
+// @ts-expect-error: file-saver doesn't have type definitions
 import { saveAs } from 'file-saver';
 import * as XLSX from 'xlsx';
 import JSZip from 'jszip';
